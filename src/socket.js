9e84c2f1cd2242103413942b6fda9b6ec7e4e1c3
Ankur sahu <ankurmaildx@gmail.com>
2023-05-10 23:40:51 +0530

    timeout connection and responsive ui fixed

--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,13 @@
 //client connection file
 import { io } from "socket.io-client";
 
-export const initSocket = async()=>{
+export const initSocket = async () => {
     const options = {
-        'force new connection' : true,
-        reconnectioAttempt : 'Infinity',
-        timeout:10000,
+        'force new connection': true,
+        reconnectionAttempt : 'Infinity',
+        timeout: 10000,
+        pingTimeout: 60000, // Set the timeout to 60 seconds
+        pingInterval: 25000, // Set the interval to 25 seconds
         transports: ['websocket'],
     }
     return io(process.env.REACT_APP_BACKEND_URL, options)
